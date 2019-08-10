c01ce5df44b3e1d2ffa9382ee50c0750135ff473
Yury Savin <yury@savin.dev>
2019-08-10 23:31:43 +0300

    Add `Page` component

---
+++ src/components/Page/Page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Layout } from 'antd';
+import Header from '../Header/Header';
+
+const Page: React.FC = ({ children }) => {
+  return (
+    <Layout>
+      <Header />
+      <Layout.Content>{children}</Layout.Content>
+      <Layout.Footer></Layout.Footer>
+    </Layout>
+  );
+};
+
+export default Page;
