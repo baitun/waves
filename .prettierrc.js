e98a13e8198aae3ed7f4ec6bb3188116d6644e32
Yury Savin <yury@savin.dev>
2019-08-10 15:13:13 +0300

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  tabWidth: 2,
+};
---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'antd/es/button';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
+
+export default App;
