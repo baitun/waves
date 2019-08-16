348ab413934cfa86afb7d68f9f9563f7d6d942bf
Yury Savin <yury@savin.dev>
2019-08-16 22:16:55 +0300

    Make shure keeper called after load

--- src/utils/tmpSimpleKeeper.ts
+++ src/utils/tmpSimpleKeeper.ts
@@ -1,17 +1,22 @@
 /** This is temporary solution! I need to use KeeperHelper class instead */
 
-import { IWavesKeeperOptions } from './keeper';
+import { IWavesKeeperOptions, IPublicState } from './keeper';
 
 declare const WavesKeeper: { initialPromise: Promise<IWavesKeeperOptions> };
 
-export function tmpKeeperInit() {
-  if (typeof WavesKeeper === 'undefined') {
-    return Promise.reject('Keeper is not installed!');
-  }
+export function tmpKeeperInit(): Promise<IPublicState> {
+  return new Promise((resolve, reject) => {
+    window.onload = function() {
+      if (typeof WavesKeeper === 'undefined') {
+        return reject('Keeper is not installed!');
+      }
 
-  return WavesKeeper.initialPromise.then((keeperApi: IWavesKeeperOptions) => {
-    return keeperApi.publicState().then((state) => {
-      return state;
-    });
+      WavesKeeper.initialPromise.then((keeperApi: IWavesKeeperOptions) => {
+        keeperApi.publicState().then((state) => {
+          console.log({ state });
+          resolve(state);
+        });
+      });
+    };
   });
 }
