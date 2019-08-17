5d484753e2d48a4e043f899e6c78c70944153f47
Yury Savin <yury@savin.dev>
2019-08-17 20:53:39 +0300

    Butify DetailsTable

--- src/components/DetailsTable/DetailsTable.tsx
+++ src/components/DetailsTable/DetailsTable.tsx
@@ -13,6 +13,37 @@ const columns: ColumnProps<any>[] = [
 ];
 
 export const DetailsTable: React.FC<DetailsTableProps> = ({ item }) => {
-  const data = Object.entries(item).map(([key, value]) => ({ key, value }));
-  return <Table columns={columns} dataSource={data} pagination={false}></Table>;
+  const map = {
+    'Auction id ': item.id,
+    'Max bid ': item.deposit,
+    'Amount ': item.lot_amount,
+    'Lot id ': item.lot_assetId,
+    'Organizer address ': item.organizer,
+    'Currency ': item.priceAssetId,
+    'Start bid ': item.startPrice,
+    'Time left for bids ': '@TODO',
+    'Second price': item.second_price
+      ? item.second_price
+      : 'Will be known after time left for bids complete',
+    'Top price ': item.top_price
+      ? item.top_price
+      : 'Will be known after time left for bids completed',
+    'Unrevealed count': item.unrevealed_count
+      ? item.unrevealed_count
+      : 'Will be known after time left for bids completed',
+    'Time left for reveal bids': '@TODO closing_start - today',
+    Settle: item.settle
+      ? 'Winner took the prize'
+      : 'The prize is held by the organizer',
+  };
+  const data = Object.entries(map).map(([key, value]) => ({ key, value }));
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+      showHeader={false}
+    ></Table>
+  );
 };
