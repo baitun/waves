906421afcaeb4dba9a588b3acda4897a63148938
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-18 23:17:00 +0000

    Deploy baitun/waves to github.com/baitun/waves.git:gh-pages

---
+++ precache-manifest.723ee60d7200150baf8e1b130093dc0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecff9a3b9ed5c6fd97ea5adc47b141fe",
+    "url": "/waves/index.html"
+  },
+  {
+    "revision": "ba61ff7f6324add155d8",
+    "url": "/waves/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "d5179072b7148c539016",
+    "url": "/waves/static/css/main.a7db9a86.chunk.css"
+  },
+  {
+    "revision": "ba61ff7f6324add155d8",
+    "url": "/waves/static/js/2.76c27ed4.chunk.js"
+  },
+  {
+    "revision": "d5179072b7148c539016",
+    "url": "/waves/static/js/main.3cd90b87.chunk.js"
+  },
+  {
+    "revision": "f06183afdc8cc1720fda",
+    "url": "/waves/static/js/runtime~main.560d0816.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/waves/precache-manifest.3d8f454064be2f0bb9535b30e164e7f2.js"
+  "/waves/precache-manifest.723ee60d7200150baf8e1b130093dc0e.js"
 );
 
 self.addEventListener('message', (event) => {
